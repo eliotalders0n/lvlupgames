fcdc254e7d79af9b4c1de00e331dbe36b42fed79
Chisomo Mwanza <13213171@AFRICA.AIRTEL.ITM>
2021-08-02 20:19:26 +0200

    getting started

--- comp/search/Search.js
+++ comp/search/Search.js
@@ -1,4 +1,4 @@
-import React from "react";
+geimport React, {useEffect} from "react";
 import {
   TextInput,
   StyleSheet,
@@ -8,11 +8,34 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { SIZES, COLORS, FONTS } from "../../constants";
+import * as Notifications from 'expo-notifications'
+import * as Permissions from 'expo-permissions'
 
 function Search(props) {
+useEffect(()=>{
+  registerForPushNOtifcations().then(token=>console.log(token).catch((e)=>{
+    console.log(e)
+  }))
+},[])
+
+  async function registerForPushNOtifcations(){
+    const{status} = await Permissions.getAsync(Permissions.NOTIFICATIONS)
+    if(status!='granted'){
+      const {status} = await Permissions.askAsync(Permissions.NOTIFICATIONS)
+    }
+    if(status!='granted')
+    {
+      alert("Failed to get access")
+      return
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data
+    return token
+
+  }
+
   return <View>
 
-    
+
   </View>;
 }
 
